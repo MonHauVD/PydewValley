e3bb1799d90a6386505de810853b62fc8c43ebcb
Nguyen Tung Duong <sieunhan283@gmail.com>
2023-11-11 21:17:13 +0700

    volume + exit

--- code/main_menu.py
+++ code/main_menu.py
@@ -1,8 +1,9 @@
 import pygame
 from settings import *
 from timer import Timer
+import sys
 
-menu_list = ['Volume', 'Close and Exit']
+menu_list = ['Volume', 'Save and Exit']
 
 
 class MainMenu:
@@ -19,6 +20,9 @@ class MainMenu:
         self.space = 10
         self.padding = 8
 
+        # volume
+        self.volume = 100
+
         # entries
         self.optionsItem = list(self.player.item_inventory.keys())
         self.optionsSeed = list(self.player.seed_inventory.keys())
@@ -29,28 +33,13 @@ class MainMenu:
 
         # movement
         self.index = 0
-        self.timer = Timer(200)
+        self.timer = Timer(50)
 
     def setup(self):
 
         # create the text surfaces
         self.text_surfs = []
         self.total_height = 0
-
-        # for item in self.options:
-        # 	text_surf = self.font.render(item, False, 'Black')
-        # 	self.text_surfs.append(text_surf)
-        # 	self.total_height += text_surf.get_height() + (self.padding * 2)
-
-        # for item in self.optionsItem:
-        # 	text_surf = self.font.render(item, False, 'Black')
-        # 	self.text_surfs.append(text_surf)
-        # 	self.total_height += text_surf.get_height() + (self.padding * 2)
-
-        # for item in self.optionsSeed:
-        # 	text_surf = self.font.render("Seed " + item, False, 'Black')
-        # 	self.text_surfs.append(text_surf)
-        # 	self.total_height += text_surf.get_height() + (self.padding * 2)
 
         for item in menu_list:
             text_surf = self.font.render(item, False, 'Black')
@@ -62,26 +51,43 @@ class MainMenu:
         self.main_rect = pygame.Rect(
             SCREEN_WIDTH / 2 - self.width / 2, self.menu_top, self.width, self.total_height)
 
-        # buy / sell text surface
+        # volume text surface
+        self.volume_text = self.font.render(f'{self.volume}%', False, 'Black')
 
     def input(self):
-        keys = pygame.key.get_pressed()
         self.timer.update()
+        for event in pygame.event.get():
+            if not self.timer.active:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.toggle_menu()
+                    elif event.key == pygame.K_DOWN:
+                        self.index += 1
+                        self.timer.activate()
+                    elif event.key == pygame.K_UP:
+                        self.index -= 1
+                        self.timer.activate()
 
-        if keys[pygame.K_ESCAPE]:
-            self.toggle_menu()
+                if self.index == 0:
+                    if event.key == pygame.K_LEFT:
+                        self.volume -= 4
+                        self.timer.activate()
+                    if event.key == pygame.K_RIGHT:
+                        self.volume += 4
+                        self.timer.activate()
 
-        if not self.timer.active:
-            if keys[pygame.K_UP]:
-                self.index -= 1
-                self.timer.activate()
+                if self.index == 1:
+                    if event.key == pygame.K_SPACE:
+                        self.timer.activate()
+                        pygame.quit()
+                        sys.exit()
 
-            if keys[pygame.K_DOWN]:
-                self.index += 1
-                self.timer.activate()
-
-            if keys[pygame.K_SPACE]:
-                self.timer.activate()
+                if event.type == pygame.KEYUP:
+                    pass
+        if self.index >= len(menu_list):
+            self.index -= 1
+        if self.index < 0:
+            self.index += 1
 
     def show_entry(self, text_surf, amount, top, selected):
 
@@ -98,6 +104,10 @@ class MainMenu:
         # selected
         if selected:
             pygame.draw.rect(self.display_surface, 'black', bg_rect, 4, 4)
+            if self.index == 0:
+                pos_rect = self.volume_text.get_rect(
+                    midleft=(self.main_rect.right - 75, bg_rect.centery))
+                self.display_surface.blit(self.volume_text, pos_rect)
 
     def update(self):
         self.input()
